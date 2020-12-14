e51b1934b87283d4f43d516062984b3cfaad88eb
darmsDD <ivandinizdobbin2@gmail.com>
2020-12-14 00:48:54 -0300

    Create script to remove server.id

---
+++ api/example.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Set the filename
+filename='tmp/pids/server.pid'
+# Create an empty file
+# Check the file is exists or not
+if [ -f $filename ]; then
+   ls
+   rm -rf tmp
+   echo "$filename is removed"
+   ls 
+else
+    ls tmp/pids/server.pid
+
+    echo "mas aaaaaaque odio jesus"
+    echo "mas que odio jesus"
+    echo "mas que odio jesus"
+    echo "mas que odio jesus"
+    echo "mas que odio jesus"
+    echo "mas que odio jesus"
+    echo "mas que odio jesus"
+    echo "mas que odio jesus"
+    echo "mas que odio jesus"
+
+echo "nao sei"
+echo "nao sei"
+echo "nao sei"
+echo "nao sei"
+ls tmp/
+
+fi
+
+
